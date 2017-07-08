256ad95b1a201e608d21fc26e3a991ff991993e9
Shannon Appelcline <shannona@skotos.net>
2017-07-08 02:09:59 -0700

    Correctly uses getrawtransaction

--- txid2bech32/txid2bech32.c
+++ txid2bech32/txid2bech32.c
@@ -94,13 +94,13 @@ int main(int argc, char *argv[]) {
     json_decref(lu_result);
     json_decref(lu_response);
 
-    /* 4. Retrieve the hash and height */
+    /* 4. Retrieve the hash */
 
-    rpc_method = bitcoinrpc_method_init(BITCOINRPC_METHOD_GETTRANSACTION);
+    rpc_method = bitcoinrpc_method_init(BITCOINRPC_METHOD_GETRAWTRANSACTION);
     
     if (!rpc_method) {
 
-      printf("ERROR: Unable to initialize gettransaction method!\n");
+      printf("ERROR: Unable to initialize getrawtransaction method!\n");
       exit(-1);
 
     }
@@ -108,15 +108,16 @@ int main(int argc, char *argv[]) {
     json_t *params = NULL;
     params = json_array();
     json_array_append_new(params,json_string(tx_id));
-
+    json_array_append_new(params,json_integer(1));
+    
     if (bitcoinrpc_method_set_params(rpc_method, params) != BITCOINRPCE_OK) {
 
-      fprintf (stderr, "Error: Could not set params for gettransaction");
+      fprintf (stderr, "Error: Could not set params for getrawtransaction");
 
     }
 
     json_decref(params);
-    
+
     bitcoinrpc_call(rpc_client, rpc_method, btcresponse, &btcerror);
 
     if (btcerror.code != BITCOINRPCE_OK) {
@@ -129,30 +130,25 @@ int main(int argc, char *argv[]) {
     lu_response = bitcoinrpc_resp_get (btcresponse);
     lu_result = json_object_get(lu_response,"result");
 
+
     if (json_is_null(lu_result)) {
 
-      printf("Error: gettransaction\n");
+      printf("Error: getrawtransaction\n");
       printf ("%s\n", json_dumps (lu_response, JSON_INDENT(2)));
       exit(-1);
       
     }
-    
+
     json_t *lu_hash = NULL;
-    const char *blockhash = 0;    
+    const char *blockhash = 0;
     lu_hash = json_object_get(lu_result,"blockhash");
     blockhash = strdup(json_string_value(lu_hash));
     json_decref(lu_hash);
-
-    json_t *lu_index = NULL;
-    int blockindex = 0;
-    lu_index = json_object_get(lu_result,"blockindex");
-    blockindex = json_integer_value(lu_index);
-    json_decref(lu_index);
-
+    
     json_decref(lu_result);
     json_decref(lu_response);
 
-    /* 5. Look up the blockheight */
+    /* 5. Look up the blockheight and index */
 
     rpc_method = bitcoinrpc_method_init(BITCOINRPC_METHOD_GETBLOCK);
     
@@ -215,6 +211,41 @@ int main(int argc, char *argv[]) {
       exit(-1);
 
     }
+
+    json_t *lu_index = NULL;
+    int blockindex = 0;
+    int foundit = 0;
+    
+    lu_index = json_object_get(lu_result,"tx");
+
+    if (json_is_null(lu_index)) {
+
+      printf("Error: getblock - no tx index\n");
+      printf ("%s\n", json_dumps (lu_index, JSON_INDENT(2)));
+      exit(-1);
+      
+    }
+      
+    for (blockindex = 0 ; blockindex < json_array_size(lu_index) ; blockindex++) {
+
+      json_t *lu_data = NULL;
+      lu_data = json_array_get(lu_index, blockindex);
+      const char *thisindex = json_string_value(lu_data);
+      
+      if (strcmp(thisindex,tx_id) == 0) {
+        foundit = 1;
+	break;
+      }
+      
+    }
+
+    if (!foundit) {
+      printf("Error: Could not find transaction in correct block!\n");
+      exit(-1);
+    }
+    
+    json_decref(lu_index);
+
 
     /* 6. Finally, do the conversion! */
 
